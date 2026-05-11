b2b7ac63fee07dae8adc4229e2e936f34de79fc8
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-11 20:19:09 +0100

    cleanup

--- factorial/app/Main.hs
+++ factorial/app/Main.hs
@@ -14,14 +14,14 @@ main =
   putStrLn "Printing  factorial"
     >> putStrLn ("enter number for factorial, but do not exceed " ++ show factorialLimit)
     >> getLine >>= \readLimit ->
-      putStrLn ("thank you for entering " ++ readLimit)
-        >> let limit = (read readLimit :: Integer)
-            in if (read readLimit :: Integer) < 1
-                 then putStrLn "the limist must be 1 or more"
-                 else
-                   if limit <= factorialLimit
-                     then
-                       putStrLn "the limit looks Ok"
-                         >> putStrLn ("going  to calculate factorial for " ++ readLimit)
-                         >> putStrLn ("calculated factorial is: " ++ show (factorial limit))
-                     else putStrLn "you exceeded the limit"
+      let limit = (read readLimit :: Integer)
+       in putStrLn ("thank you for entering " ++ readLimit)
+            >> if limit < 1
+              then putStrLn "the limist must be 1 or more"
+              else
+                if limit > factorialLimit
+                  then putStrLn "you exceeded the limit"
+                  else
+                    putStrLn "the limit looks Ok"
+                      >> putStrLn ("going  to calculate factorial for " ++ readLimit)
+                      >> putStrLn ("calculated factorial is: " ++ show (factorial limit))
