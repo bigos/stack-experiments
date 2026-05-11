bc33e33957a32cb9550945702cee0bffed565276
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-11 22:12:02 +0100

    cleanup

--- factorial-do/app/Main.hs
+++ factorial-do/app/Main.hs
@@ -10,18 +10,21 @@ factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
 main :: IO ()
-main =
+main = do
   putStrLn "Printing  factorial"
-    >> putStrLn ("enter number for factorial, but do not exceed " ++ show factorialLimit)
-    >> getLine >>= \readLimit ->
-      let limit = (read readLimit :: Integer)
-       in putStrLn ("thank you for entering " ++ readLimit)
-            >> if limit < 1
-              then putStrLn "the limit must be 1 or more"
+  putStrLn ("enter number for factorial, but do not exceed " ++ show factorialLimit)
+  readLimit <- getLine
+  let limit = (read readLimit :: Integer)
+   in putStrLn ("thank you for entering " ++ readLimit)
+        >> if limit < 1
+          then putStrLn "the limit must be 1 or more"
+          else
+            if limit > factorialLimit
+              then putStrLn "you exceeded the limit"
               else
-                if limit > factorialLimit
-                  then putStrLn "you exceeded the limit"
-                  else
-                    putStrLn "the limit looks Ok"
-                      >> putStrLn ("going  to calculate factorial for " ++ readLimit)
-                      >> putStrLn ("calculated factorial is: " ++ show (factorial limit))
+                putStrLn
+                  "the limit looks Ok"
+                  >> putStrLn
+                    ("going  to calculate factorial for " ++ readLimit)
+                  >> putStrLn
+                    ("calculated factorial is: " ++ show (factorial limit))
