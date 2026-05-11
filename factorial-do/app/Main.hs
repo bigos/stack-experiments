1e47e8c38d039b03cdf26b0e08fd0beabcaacc7b
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-12 00:54:14 +0100

    next stage

--- factorial-do/app/Main.hs
+++ factorial-do/app/Main.hs
@@ -13,7 +13,7 @@ factorial n = n * factorial (n - 1)
 validateLimit :: Integer -> Either String Integer
 validateLimit limit
   | limit < 1 = Left "The limit must be one or more"
-  | limit > factorialLimit = Left "ou exceeded te limit"
+  | limit > factorialLimit = Left "You exceeded te limit"
   | otherwise = Right limit
 
 main :: IO ()
@@ -22,9 +22,11 @@ main = do
   putStrLn ("enter number for factorial, but do not exceed " ++ show factorialLimit)
   readLimit <- getLine
   putStrLn ("thank you for entering " ++ readLimit)
-  case validateLimit (read readLimit :: Integer) of -- Warning this will crash the program if the read fails!
-    Left errorMessage -> putStrLn errorMessage
-    Right limit -> do
-      putStrLn "the limit looks Ok"
-      putStrLn ("going  to calculate factorial for " ++ readLimit)
-      putStrLn ("calculated factorial is: " ++ show (factorial limit))
+  case (readMaybe readLimit :: Maybe Integer) of
+    Nothing -> putStrLn "You forgot to enter correct number"
+    Just numberLimit -> case validateLimit numberLimit of
+      Left errorMessage -> putStrLn errorMessage
+      Right limit -> do
+        putStrLn "the limit looks Ok"
+        putStrLn ("going  to calculate factorial for " ++ readLimit)
+        putStrLn ("calculated factorial is: " ++ show (factorial limit))
