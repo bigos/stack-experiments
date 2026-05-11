c9bf0e146fad0a26704135b7790ef05fc4d113c9
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-11 19:10:32 +0100

    little factorial project

--- factorial/app/Main.hs
+++ factorial/app/Main.hs
@@ -3,17 +3,26 @@ module Main (main) where
 -- import Lib
 
 factorialLimit :: Integer
-factorialLimit = 5
+factorialLimit = 15
+
+myFactorial :: Integer -> Integer
+myFactorial num = factorial num
+  where
+    factorial 0 = 1
+    factorial n = n * factorial (n - 1)
 
 main :: IO ()
 main =
   putStrLn "Printing  factorial"
     >> putStrLn ("enter number for factorial, but do not exceed " ++ show factorialLimit)
-    >> getLine >>= \readFactorialLimit ->
-      (read readFactorialLimit :: Integer)
-        >>= \intFactorialLimit ->
-          if (intFactorialLimit < 1)
-            then putStrLn "the limit must be greater than 1"
-            else
-              putStrLn
-                ("thank you for entering " ++ readFactorialLimit)
+    >> getLine >>= \theLimit ->
+      putStrLn ("thank you for entering " ++ theLimit)
+        >> if (read theLimit :: Integer) < 1
+          then putStrLn "the limist must be 1 or more"
+          else
+            if (read theLimit :: Integer) <= factorialLimit
+              then
+                putStrLn "the limit looks Ok"
+                  >> putStrLn ("going  to calculate factorial for " ++ theLimit)
+                  >> putStrLn ("calculated factorial is: " ++ show (myFactorial (read theLimit :: Integer)))
+              else putStrLn "you exceeded the limit"
