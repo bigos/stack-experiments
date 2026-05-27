06a595985de2dc1cecd779da1caa3c3558853964
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2026-05-27 11:56:58 +0100

    playing with lambda where we have let

--- more-on-functions/app/Main.hs
+++ more-on-functions/app/Main.hs
@@ -1,6 +1,26 @@
 module Main (main) where
 
-import Lib
+import Prelude (Float, IO, String, foldl, print, putStrLn, read, show, (+))
+
+sumStrLet :: [String] -> Float
+sumStrLet =
+  let addStr x str = x + read str
+   in foldl addStr 0.0
+
+sumStrWhere :: [String] -> Float
+sumStrWhere = foldl addStr 0.0
+  where
+    addStr x str =
+      x + read str
+
+sumStrLambda :: [String] -> Float
+sumStrLambda = foldl (\a str -> a + read str) 0.0
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Play with those functions"
+  print (show (sumStrLet ["1", "2"]))
+  putStrLn " --- "
+  print (show (sumStrWhere ["3", "4"]))
+  putStrLn " --- "
+  print (show (sumStrLambda ["5", "6"]))
