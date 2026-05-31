9de2e6fba425dae2ccb5ee0ad5bbf614086c07c1
Jacek Podkanski <ruby.object@googlemail.com>
2026-05-31 16:22:39 +0100

    comments on gthci use

--- using-ghci-effectively/app/Main.hs
+++ using-ghci-effectively/app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+-- import Lib
+
+-- my variant uses Emacs REPL
+-- after loading the file
+
+-- tab completion works
+-- :commads work
+-- :set +s enables function timing
+-- multiline experiment in repl did not work with :{ }:
+-- but multiline with Ctrl-j works
 
 main :: IO ()
-main = someFunc
+main = putStrLn "Using GHCi effectively"
